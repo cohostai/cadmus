9b57fb16118e215b3bffe5142e597fbbd933d9c4
thuync <nguyenchungthuy.hust@gmail.com>
2018-09-06 16:56:53 +0700

    Adjust apspec

--- integrations/indexer/main.py
+++ integrations/indexer/main.py
@@ -49,7 +49,7 @@ def get_es_client():
         config.ELASTICSEARCH_REGION,
         'es'
     )
-    
+
     return Elasticsearch(
         hosts=[config.ELASTICSEARCH_HOST],
         http_auth=awsauth,
