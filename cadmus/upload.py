bfc57081269997db11328b3dba430289111c0d33
doankhoi <doanngockhoi93@gmail.com>
2018-12-20 17:14:58 +0700

    Support send image message airbnb

--- cadmus/upload.py
+++ cadmus/upload.py
@@ -16,10 +16,11 @@ from flask import request
 from jose import JWTError
 from werkzeug.utils import secure_filename
 
-from .auth import NotFoundError
-from .config import AppConfig
 from . import response
 from . import s3
+from .auth import Auth
+from .auth import NotFoundError
+from .config import AppConfig
 
 ALLOWED_EXTENSIONS = [
     ".jpg",
@@ -30,6 +31,12 @@ ALLOWED_EXTENSIONS = [
     ".svg",
     ".bmp"
 ]
+
+auth = Auth(
+    issuer='https://%s/' % AppConfig.AUTH0_AUTH_DOMAIN,
+    api_audience=AppConfig.COHOST_API_AUDIENCE,
+    jwks_url=AppConfig.COHOST_JWKS_URL
+)
 
 app = Flask(__name__)
 app.config.from_object(AppConfig)
@@ -57,7 +64,7 @@ def not_found_user(e):
 
 # TODO: make streaming upload
 # TODO: validate client id
-@app.route('/upload/user', methods=['POST'])
+@app.route('/pictures/user', methods=['POST'])
 # @auth.require_auth
 def upload_user_images():
     if "file" not in request.files:
@@ -72,6 +79,28 @@ def upload_user_images():
 
     fileobj.filename = secure_filename(fileobj.filename)
     key_prefix = "pictures/user"
+    url = s3.upload_fileobj(fileobj, key_prefix=key_prefix)
+
+    return jsonify({
+        "url": url
+    })
+
+
+@app.route('/pictures/message', methods=['POST'])
+@auth.require_auth
+def upload_message_image():
+    if "file" not in request.files:
+        return response.no_file_provided()
+
+    fileobj = request.files["file"]
+    if not fileobj.filename:
+        return response.no_file_provided()
+
+    if not allowed_file(fileobj):
+        return response.invalid_file_type()
+
+    fileobj.filename = secure_filename(fileobj.filename)
+    key_prefix = "pictures/message"
     url = s3.upload_fileobj(fileobj, key_prefix=key_prefix)
 
     return jsonify({
